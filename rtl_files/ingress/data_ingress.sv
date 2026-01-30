4f95fb1d866a8c925058c5870687d08aba1586ac
xVect0r <soumyadip02.roy@gmail.com>
2026-01-30 08:06:52 +0530

    INITIAL COMMIT

--- rtl_files/ingress/data_ingress.sv
+++ rtl_files/ingress/data_ingress.sv
@@ -1,4 +1,3 @@
-// Data Ingress Module
 module data_ingress #(
     parameter int DATA_W = 32
 ) (
@@ -6,24 +5,17 @@ module data_ingress #(
     input logic rst,
     axi_if.master m_axi
 );
-
-    // Internal Sample Generators
-
     logic [15:0] ch0_count;
     logic [15:0] ch1_count;
 
-    // -- AXI Holding Registers
     logic tvalid_reg;
     logic [DATA_W-1:0] tdata_reg;
 
-    // AXI assignments
 
     assign m_axi.tvalid = tvalid_reg;
     assign m_axi.tdata = tdata_reg;
     assign m_axi.tlast = 1'b0;
     assign m_axi.tuser = '0;
-
-    //AXI Source Behaviour
 
     always_ff @( posedge clk ) begin 
         if (rst) begin
@@ -40,6 +32,7 @@ module data_ingress #(
                 ch0_count<= ch0_count+16'd1;
                 ch1_count<= ch1_count+16'd1;
 
+
             end
 
             else if(tvalid_reg && m_axi.tready) begin
@@ -47,7 +40,6 @@ module data_ingress #(
                 tvalid_reg<=1'b1;
                 ch0_count<= ch0_count+16'd1;
                 ch1_count<= ch1_count+16'd1;
-
             end
         end        
 end
